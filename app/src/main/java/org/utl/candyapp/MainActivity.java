cd2035dead6686919557debccdcde31904c5cd81
MarioZuniga25 <mariogt367@gmail.com>
2023-03-19 17:47:10 -0600

    initial commit

---
+++ app/src/main/java/org/utl/candyapp/MainActivity.java
@@ -0,0 +1,39 @@
+package org.utl.candyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtUsuario, txtContrasenia;
+    Button btnLogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtUsuario = findViewById(R.id.txtUsuario);
+        txtContrasenia = findViewById(R.id.txtContrasenia);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+    }
+
+
+
+}
